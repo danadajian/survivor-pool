91c998c480ab72984a2f582268c6ec5d8cb964b4
danadajian <danadajian@gmail.com>
2023-09-12 09:33:37 -0500

    add env var to ci

--- src/backend/fetch-games.test.ts
+++ src/backend/fetch-games.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, mock } from "bun:test";
+import { describe, expect, it, mock } from "bun:test";
 import { fetchGames } from "./fetch-games";
 import { mockEspnResponse } from "./mock-espn-response";
 
@@ -7,10 +7,6 @@ const mockFetch = mock(fetch).mockResolvedValue(
 );
 
 describe("test", () => {
-  beforeEach(() => {
-    process.env.GAMES_API_URL = "https://some-api-url";
-  });
-
   it("should parse api response", async () => {
     const result = await fetchGames(mockFetch);
     expect(result).toEqual(mockEspnResponse);
