dd348472256706275f4c2c4f83c3c831d5274088
danadajian <danadajian@gmail.com>
2023-09-13 12:33:24 -0500

    upsert

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -2,24 +2,13 @@ import postgres from "postgres";
 import { environmentVariables } from "./env";
 import { randomUUID } from "crypto";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { and, desc, eq } from "drizzle-orm";
+import { sql, desc, eq } from "drizzle-orm";
 import * as schema from "./schema";
 import { picks } from "./schema";
 import type { makePickInput } from "./router";
 
 export const pgClient = postgres(environmentVariables.POSTGRES_URL);
 export const db = drizzle(pgClient, { schema });
-
-export async function makePick({
-  username,
-  teamPicked,
-  week,
-  season,
-}: typeof makePickInput.infer) {
-  return db
-    .insert(picks)
-    .values({ id: randomUUID(), username, teamPicked, week, season });
-}
 
 export async function fetchPickForUser({
   username,
@@ -31,10 +20,25 @@ export async function fetchPickForUser({
   season: number;
 }) {
   return db.query.picks.findFirst({
-    where: and(
-      and(and(eq(picks.username, username), eq(picks.week, week))),
-      eq(picks.season, season),
-    ),
+    where: sql`username = ${username} and week = ${week} and season = ${season}`,
     orderBy: desc(picks.timestamp),
   });
+}
+
+export async function makePick({
+  username,
+  teamPicked,
+  week,
+  season,
+}: typeof makePickInput.infer) {
+  const existingPick = await fetchPickForUser({ username, week, season });
+  if (existingPick) {
+    return db
+      .update(picks)
+      .set({ teamPicked })
+      .where(eq(picks.id, existingPick.id));
+  }
+  return db
+    .insert(picks)
+    .values({ id: randomUUID(), username, teamPicked, week, season });
 }
