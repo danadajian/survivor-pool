78782ad44d1420000f71da511f798763b5effa35
danadajian <danadajian@gmail.com>
2023-09-13 21:38:01 -0500

    refactor and add pool dbs

--- src/backend/schema.ts
+++ src/backend/schema.ts
@@ -1,19 +1,31 @@
 import {
   integer,
   pgTable,
+  serial,
   timestamp,
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
 
 export const picks = pgTable("picks", {
-  id: uuid("id").notNull().primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   username: varchar("username", { length: 256 }).notNull(),
   teamPicked: varchar("team_picked", { length: 256 }).notNull(),
   week: integer("week").notNull(),
   season: integer("season").notNull(),
-  timestamp: timestamp("timestamp")
-    .notNull()
-    .default(sql`current_timestamp`),
+  poolId: integer("pool_id"),
+  timestamp: timestamp("timestamp").defaultNow(),
+});
+
+export const pools = pgTable("pools", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  creator: varchar("creator", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+export const poolMembers = pgTable("poolMembers", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: varchar("username", { length: 256 }).notNull(),
+  poolId: integer("pool_id").notNull(),
 });
---
+++ src/frontend/team-button.tsx
@@ -0,0 +1,91 @@
+import { trpc } from "./trpc";
+import { useState } from "react";
+import { DialogWrapper } from "./dialog-wrapper";
+import { Dialog } from "@headlessui/react";
+import type { Event } from "./app";
+
+type Team = Event["competitions"][number]["competitors"][number]["team"];
+type TeamProps = { team?: Team; teamPicked?: string; username: string };
+export const TeamButton = ({ team, teamPicked, username }: TeamProps) => {
+  const context = trpc.useContext();
+  const gamesAndPicks = context.gamesAndPicks.getData({ username });
+  const { mutateAsync } = trpc.makePick.useMutation();
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const toggleDialog = () => setDialogIsOpen(!dialogIsOpen);
+
+  if (!gamesAndPicks || !team) return <div>No team found.</div>;
+  const handleUpdate = async () => {
+    await mutateAsync({
+      username,
+      teamPicked: team.name,
+      week: gamesAndPicks.games.week.number,
+      season: gamesAndPicks.games.season.year,
+    });
+    await context.gamesAndPicks.invalidate();
+    toggleDialog();
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }, 50);
+  };
+  const { forbiddenTeams } = gamesAndPicks;
+  const teamCurrentlyPicked = team.name === teamPicked;
+  const teamPreviouslyPicked = forbiddenTeams?.includes(team.name);
+  const buttonClass = teamCurrentlyPicked
+    ? "bg-blue-800 text-white"
+    : teamPreviouslyPicked
+    ? "bg-blue-800 text-white opacity-30"
+    : "bg-slate-300";
+  const imageClass = teamPreviouslyPicked ? "opacity-80" : "";
+  return (
+    <>
+      <button
+        /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+        // @ts-ignore
+        disabled={teamCurrentlyPicked || teamPreviouslyPicked}
+        onClick={toggleDialog}
+        className={`flex flex-col items-center rounded-lg border-2 border-slate-100 p-2 ${buttonClass}`}
+      >
+        <img
+          alt={team.abbreviation}
+          src={team.logo}
+          className={`h-14 w-14 ${imageClass}`}
+        />
+        <p>{team.name}</p>
+      </button>
+      <DialogWrapper dialogIsOpen={dialogIsOpen} toggleDialog={toggleDialog}>
+        <>
+          <Dialog.Title
+            as="h3"
+            className="pt-2 text-xl font-semibold leading-6"
+          >
+            Confirm pick
+          </Dialog.Title>
+          <Dialog.Description className="pt-5 font-semibold text-slate-500">
+            Are you sure you want to pick the {team.name}? You won't be able to
+            pick them again this season.
+          </Dialog.Description>
+
+          <div className="flex justify-end pt-5">
+            <button
+              className="rounded-md bg-blue-800 px-3 py-2 font-medium uppercase text-white hover:bg-blue-500"
+              autoFocus
+              onClick={handleUpdate}
+            >
+              Lock it in
+            </button>
+            <button
+              className="ml-3 rounded-md border border-blue-800 px-3 py-2 font-medium uppercase text-blue-800 hover:bg-blue-300"
+              onClick={toggleDialog}
+            >
+              Cancel
+            </button>
+          </div>
+        </>
+      </DialogWrapper>
+    </>
+  );
+};
