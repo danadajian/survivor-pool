0fea956018c0ab3baa911e6c3d333114de190e1f
danadajian <danadajian@gmail.com>
2023-09-22 00:31:48 -0500

    only include tailwind script in dev mode

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -24,7 +24,9 @@ export const App = () => (
       />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Survivor Pool</title>
-      <script src="https://cdn.tailwindcss.com"></script>
+      {process.env.NODE_ENV === "development" && (
+        <script src="https://cdn.tailwindcss.com"></script>
+      )}
       <link rel="stylesheet" href="/public/globals.css" />
     </head>
     <body>
