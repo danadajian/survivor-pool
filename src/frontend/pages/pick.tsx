ffd524af807923869942da6fe131505649990942
danadajian <danadajian@gmail.com>
2023-09-25 21:08:25 -0500

    filter by poolId

--- src/frontend/pages/pick.tsx
+++ src/frontend/pages/pick.tsx
@@ -71,6 +71,13 @@ const PickContent = ({
         >
           Back to pools
         </button>
+        <button
+          onClick={() => navigate(`/create`)}
+          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+          type="button"
+        >
+          Start a new pool
+        </button>
       </>
     );
   }
