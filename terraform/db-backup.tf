9397102f3f47e247570de5f0bbbd9c6f78b83e54
danadajian <danadajian@gmail.com>
2023-09-30 15:31:44 -0500

    remove access key

--- terraform/db-backup.tf
+++ terraform/db-backup.tf
@@ -24,7 +24,3 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = aws_iam_user.db_backup_user.name
   policy_arn = aws_iam_policy.db_backup_policy.arn
 }
-
-resource "aws_iam_access_key" "access_key" {
-  user = aws_iam_user.db_backup_user.name
-}
