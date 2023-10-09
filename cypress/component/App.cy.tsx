200aaf2ed04fcb6428cacfcafca6d1dd4e257620
danadajian <danadajian@gmail.com>
2023-10-09 10:51:27 -0500

    remove date assertion

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -86,7 +86,6 @@ describe("App Tests", () => {
       cy.findByRole("heading", { name: "Make your pick, Test!" }).should(
         "be.visible",
       );
-      cy.findByRole("heading", { name: "Monday 7:15 pm" }).should("be.visible");
       cy.findByRole("heading", { name: "BUF -2.0" }).should("be.visible");
     });
 
