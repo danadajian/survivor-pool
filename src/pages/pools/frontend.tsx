f6651e11ecd63d438658044570a9d75d46fb28d2
danadajian <danadajian@gmail.com>
2023-10-18 08:13:33 -0500

    improve pre-week experience

--- src/pages/pools/frontend.tsx
+++ src/pages/pools/frontend.tsx
@@ -6,7 +6,7 @@ import { Error } from "../../components/error";
 import { Header } from "../../components/header";
 import { Loader } from "../../components/loader";
 import { type PageProps, withPage } from "../../components/page-wrapper";
-import { trpc } from "../../trpc";
+import { type RouterOutput, trpc } from "../../trpc";
 import { useUrlParams } from "../../utils/useUrlParams";
 
 const PoolsComponent = ({ user: { username } }: PageProps) => {
@@ -44,47 +44,16 @@ const AllPicks = ({
   }
   const { picks, week: currentWeek } = data;
 
-  if (!picks.length) {
-    return <Header>No picks yet for week {week}</Header>;
-  }
-
   return (
     <>
       <Header>Week {week ?? currentWeek} Picks</Header>
-      <table className="mb-8 table-auto">
-        <thead>
-          <tr>
-            <th className="px-4">User</th>
-            <th className="px-4">Team Picked</th>
-            <th className="px-4">Result</th>
-          </tr>
-        </thead>
-        <tbody>
-          {picks.map((pick, index) => {
-            const isUserRow = pick.username === username;
-            const userClasses = isUserRow ? "font-semibold text-blue-800" : "";
-            const resultClasses =
-              pick.result === "WON"
-                ? "text-green-600"
-                : pick.result === "LOST"
-                ? "text-red-600"
-                : "";
-            const userFullName =
-              pick.firstName && pick.lastName
-                ? `${pick.firstName} ${pick.lastName}`
-                : undefined;
-            return (
-              <tr key={index} className={userClasses}>
-                <td>{userFullName ?? pick.username}</td>
-                <td>{pick.teamPicked}</td>
-                <td className={`font-semibold ${resultClasses}`}>
-                  {pick.result ?? "PENDING"}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {picks.length ? (
+        <PickTable picks={picks} username={username} />
+      ) : (
+        <h2 className="pb-4 text-lg font-bold">
+          No picks yet for Week {currentWeek}
+        </h2>
+      )}
       <Dropdown
         options={Array.from({ length: currentWeek }, (_, i) => i + 1)}
         selected={Number(week)}
@@ -95,3 +64,45 @@ const AllPicks = ({
 };
 
 export const Pools = withPage(PoolsComponent);
+
+type PickTableProps = {
+  picks: RouterOutput["picksForPool"]["picks"];
+  username: string;
+};
+
+const PickTable = ({ picks, username }: PickTableProps) => (
+  <table className="mb-8 table-auto">
+    <thead>
+      <tr>
+        <th className="px-4">User</th>
+        <th className="px-4">Team Picked</th>
+        <th className="px-4">Result</th>
+      </tr>
+    </thead>
+    <tbody>
+      {picks.map((pick, index) => {
+        const isUserRow = pick.username === username;
+        const userClasses = isUserRow ? "font-semibold text-blue-800" : "";
+        const resultClasses =
+          pick.result === "WON"
+            ? "text-green-600"
+            : pick.result === "LOST"
+            ? "text-red-600"
+            : "";
+        const userFullName =
+          pick.firstName && pick.lastName
+            ? `${pick.firstName} ${pick.lastName}`
+            : undefined;
+        return (
+          <tr key={index} className={userClasses}>
+            <td>{userFullName ?? pick.username}</td>
+            <td>{pick.teamPicked}</td>
+            <td className={`font-semibold ${resultClasses}`}>
+              {pick.result ?? "PENDING"}
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+);
