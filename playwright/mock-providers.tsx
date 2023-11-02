a5d4c81abbe73705290ad42fdf1b8a95d466f975
danadajian <danadajian@gmail.com>
2023-11-02 08:19:03 -0500

    fix tests

--- playwright/mock-providers.tsx
+++ playwright/mock-providers.tsx
@@ -3,6 +3,8 @@ import type { InitialState } from "@clerk/types";
 import React, { type ComponentProps, type PropsWithChildren } from "react";
 import { MemoryRouter } from "react-router-dom";
 
+import { ClientProvider } from "../src/components/client-provider";
+
 const mockClerkState = {
   user: {
     id: "user_123",
@@ -19,10 +21,12 @@ export const MockProviders = ({
 }: PropsWithChildren & {
   initialEntries?: ComponentProps<typeof MemoryRouter>["initialEntries"];
 }) => (
-  <ClerkProvider
-    initialState={mockClerkState}
-    publishableKey="pk_test_YW11c2luZy1tYW4tNjEuY2xlcmsuYWNjb3VudHMuZGV2JA"
-  >
-    <MemoryRouter initialEntries={initialEntries}>{children}</MemoryRouter>
-  </ClerkProvider>
+  <ClientProvider>
+    <ClerkProvider
+      initialState={mockClerkState}
+      publishableKey="pk_test_YW11c2luZy1tYW4tNjEuY2xlcmsuYWNjb3VudHMuZGV2JA"
+    >
+      <MemoryRouter initialEntries={initialEntries}>{children}</MemoryRouter>
+    </ClerkProvider>
+  </ClientProvider>
 );
