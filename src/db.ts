bbbad8cb3f40dc101304a24161bc2fbc836a69a7
danadajian <danadajian@gmail.com>
2023-11-06 14:24:50 -0600

    set idle timeout in db

--- src/db.ts
+++ src/db.ts
@@ -4,5 +4,7 @@ import postgres from "postgres";
 import { environmentVariables } from "./env";
 import * as schema from "./schema";
 
-export const pgClient = postgres(environmentVariables.POSTGRES_URL);
+export const pgClient = postgres(environmentVariables.POSTGRES_URL, {
+  idle_timeout: 60,
+});
 export const db = drizzle(pgClient, { schema });
