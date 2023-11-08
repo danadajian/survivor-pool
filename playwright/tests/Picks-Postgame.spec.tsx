0231cbabe9739095436e36c4fcf4605f069d3fe5
danadajian <danadajian@gmail.com>
2023-11-08 10:39:34 -0600

    split playwright tests

--- playwright/tests/Picks-Postgame.spec.tsx
+++ playwright/tests/Picks-Postgame.spec.tsx
@@ -5,7 +5,6 @@ import { Pick } from "../../src/pages/pick/frontend";
 import {
   basicGamesAndPicksResponse,
   responseWithPick,
-  responseWithPickAndForbiddenTeams,
   responseWithPickAndResultsTeamLost,
   responseWithPickAndResultsTeamWon,
 } from "../../test/mocks";
@@ -20,80 +19,6 @@ test.beforeEach(async ({ page }) => {
         "Mon Sep 6 2023 21:50:04 GMT-0500 (Central Daylight Time)",
       ).getTime();
   });
-});
-
-test("renders without picks", async ({ mount }) => {
-  const component = await mount(
-    <MockProviders initialEntries={["/pick/123"]}>
-      <Pick />
-    </MockProviders>,
-  );
-  await expect(
-    component.getByRole("heading", { name: "Test Pool 2023" }),
-  ).toBeVisible();
-  await expect(
-    component.getByRole("heading", { name: "Week 1" }),
-  ).toBeVisible();
-  await expect(
-    component.getByRole("heading", { name: "Make your pick, Test!" }),
-  ).toBeVisible();
-  await expect(
-    component.getByRole("heading", { name: "BUF -2.0" }),
-  ).toBeVisible();
-});
-
-test("can make a pick", async ({ mount, page }) => {
-  await page.route("/trpc/makePick*", (route) =>
-    route.fulfill({
-      body: JSON.stringify({ result: { data: {} } }),
-    }),
-  );
-  const component = await mount(
-    <MockProviders initialEntries={["/pick/123"]}>
-      <Pick />
-    </MockProviders>,
-  );
-  await component.getByRole("button", { name: /49ers/ }).click();
-  await expect(
-    page.getByRole("heading", { name: "Confirm pick" }),
-  ).toBeVisible();
-  await expect(
-    page.getByText(/Are you sure you want to pick the 49ers?/),
-  ).toBeVisible();
-  await page.route("/trpc/pick*", (route) =>
-    route.fulfill({
-      body: JSON.stringify(responseWithPick),
-    }),
-  );
-  await page.getByRole("button", { name: "Lock it in" }).click();
-  await expect(component.getByRole("button", { name: /49ers/ })).toBeDisabled();
-  await expect(
-    component.getByRole("heading", {
-      name: "You're riding with the 49ers this week!",
-    }),
-  ).toBeVisible();
-});
-
-test("prevents picking the same team twice", async ({ mount, page }) => {
-  await page.route("/trpc/pick*", (route) =>
-    route.fulfill({
-      body: JSON.stringify(responseWithPickAndForbiddenTeams),
-    }),
-  );
-  const component = await mount(
-    <MockProviders initialEntries={["/pick/123"]}>
-      <Pick />
-    </MockProviders>,
-  );
-
-  await expect(component.getByRole("button", { name: /49ers/ })).toBeDisabled();
-  await expect(component.getByRole("button", { name: /Bills/ })).toBeDisabled();
-  await expect(component.getByRole("button", { name: /Jets/ })).toBeDisabled();
-  await expect(
-    component.getByRole("heading", {
-      name: "You're riding with the 49ers this week!",
-    }),
-  ).toBeVisible();
 });
 
 test("prevents changing pick after picking a game that has started", async ({
