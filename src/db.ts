9efe138b448c1d455de786b2edf22cdb41b820c2
danadajian <danadajian@gmail.com>
2023-11-13 15:45:07 -0600

    increase connection pool

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,8 @@ import { environmentVariables } from "./env";
 import * as schema from "./schema";
 
 export const pgClient = postgres(environmentVariables.POSTGRES_URL, {
-  max_lifetime: 3600,
+  max: 100,
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
 });
 export const db = drizzle(pgClient, { schema });
