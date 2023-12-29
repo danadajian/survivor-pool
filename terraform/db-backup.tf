766c178f0726cbea9922dfe57ec94c84fc337a8d
danadajian <danadajian@gmail.com>
2023-12-28 21:53:15 -0600

    rename user and policy

--- terraform/db-backup.tf
+++ terraform/db-backup.tf
@@ -3,11 +3,11 @@ resource "aws_s3_bucket" "db_backup_bucket" {
 }
 
 resource "aws_iam_user" "db_backup_user" {
-  name = "db-backup-user"
+  name = "survivor-pool-db-backup-user"
 }
 
 resource "aws_iam_policy" "db_backup_policy" {
-  name = "db-backup-policy"
+  name = "survivor-pool-db-backup-policy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
