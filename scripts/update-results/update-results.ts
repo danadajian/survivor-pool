622fc9a35c98e201cdd13ed083d91ca8c2a909b7
danadajian <danadajian@gmail.com>
2025-09-20 21:59:36 -0500

    refactor

--- scripts/update-results/update-results.ts
+++ scripts/update-results/update-results.ts
@@ -4,32 +4,6 @@ import { db } from "../../src/db";
 import { type Events } from "../../src/pages/pool/backend";
 import { members, picks } from "../../src/schema";
 
-function getResult(teamPicked: string, events: Events) {
-  const teamPickedInEvent = events.find((event) =>
-    event.competitions[0]?.competitors.some(
-      (competitor) => competitor.team.name === teamPicked,
-    ),
-  );
-  const teamPickedFromApi =
-    teamPickedInEvent?.competitions[0]?.competitors.find(
-      (competitor) => competitor.team.name === teamPicked,
-    );
-  const eventHasEnded = teamPickedFromApi && "winner" in teamPickedFromApi;
-  if (!eventHasEnded) {
-    return "PENDING";
-  }
-
-  return teamPickedFromApi.winner === true ? "WON" : "LOST";
-}
-
-async function eliminateUser(username: string, poolId: string) {
-  console.log(`Eliminating user ${username} from poolId ${poolId}...`);
-  await db
-    .update(members)
-    .set({ eliminated: true })
-    .where(and(eq(members.username, username), eq(members.poolId, poolId)));
-}
-
 export async function updateResults(
   events: Events,
   week: number,
@@ -45,22 +19,17 @@ export async function updateResults(
     .where(eq(picks.season, season));
 
   for (const { username, poolId } of membersStillAlive) {
-    const userPicksForWeek = allUserPicks.filter((pick) => pick.week === week);
-    const userPicksForPoolId = userPicksForWeek.filter(
-      (pick) => pick.poolId === poolId,
+    if (failedToPickLastWeek(allUserPicks, username, week)) {
+      await eliminateUser(username, poolId);
+      continue;
+    }
+
+    const userPicksForPoolId = allUserPicks.filter(
+      (pick) => pick.week === week && pick.poolId === poolId,
     );
     const userPick = userPicksForPoolId.find(
       (pick) => pick.username === username,
     );
-    const failedToPickLastWeek =
-      week > 1 &&
-      !allUserPicks.some(
-        (pick) => pick.username === username && pick.week < week,
-      );
-    if (failedToPickLastWeek) {
-      await eliminateUser(username, poolId);
-      continue;
-    }
     if (!userPick?.teamPicked) {
       continue;
     }
@@ -88,4 +57,41 @@ export async function updateResults(
       await eliminateUser(username, poolId);
     }
   }
+}
+
+function getResult(teamPicked: string, events: Events) {
+  const teamPickedInEvent = events.find((event) =>
+    event.competitions[0]?.competitors.some(
+      (competitor) => competitor.team.name === teamPicked,
+    ),
+  );
+  const teamPickedFromApi =
+    teamPickedInEvent?.competitions[0]?.competitors.find(
+      (competitor) => competitor.team.name === teamPicked,
+    );
+  const eventHasEnded = teamPickedFromApi && "winner" in teamPickedFromApi;
+  if (!eventHasEnded) {
+    return "PENDING";
+  }
+
+  return teamPickedFromApi.winner === true ? "WON" : "LOST";
+}
+
+async function eliminateUser(username: string, poolId: string) {
+  console.log(`Eliminating user ${username} from poolId ${poolId}...`);
+  await db
+    .update(members)
+    .set({ eliminated: true })
+    .where(and(eq(members.username, username), eq(members.poolId, poolId)));
+}
+
+function failedToPickLastWeek(
+  allUserPicks: (typeof picks.$inferSelect)[],
+  username: string,
+  week: number,
+) {
+  return (
+    week > 1 &&
+    !allUserPicks.some((pick) => pick.username === username && pick.week < week)
+  );
 }
