86524fe8350eb64a4c63c0084539a4bb16ccc341
danadajian <danadajian@gmail.com>
2025-09-22 10:27:57 -0500

    refactor

--- src/trpc.ts
+++ src/trpc.ts
@@ -1,9 +1,6 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AnyRouter, inferRouterOutputs } from "@trpc/server";
-import {
-  type FetchHandlerRequestOptions,
-  fetchRequestHandler,
-} from "@trpc/server/adapters/fetch";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { type Elysia } from "elysia";
 
 import { createContext } from "./context";
@@ -13,41 +10,31 @@ export const trpc = createTRPCReact<AppRouter>();
 
 export type RouterOutput = inferRouterOutputs<AppRouter>;
 
-type TRPCOptions = {
-  endpoint?: string;
-} & Omit<FetchHandlerRequestOptions<AnyRouter>, "req" | "router" | "endpoint">;
-
-export const trpcRouter =
-  (
-    router: AnyRouter,
-    { endpoint = "/trpc", ...options }: TRPCOptions = { endpoint: "/trpc" },
-  ) =>
-  (app: Elysia) => {
-    return app
-      .get(
-        `${endpoint}/*`,
-        async ({ request }) => {
-          return fetchRequestHandler({
-            ...options,
-            req: request,
-            createContext,
-            router,
-            endpoint,
-          });
-        },
-        { parse: "none" },
-      )
-      .post(
-        `${endpoint}/*`,
-        async ({ request }) => {
-          return fetchRequestHandler({
-            ...options,
-            req: request,
-            createContext,
-            router,
-            endpoint,
-          });
-        },
-        { parse: "none" },
-      );
-  };
+export const trpcRouter = (router: AnyRouter) => (app: Elysia) => {
+  const endpoint = "trpc";
+  return app
+    .get(
+      `${endpoint}/*`,
+      async ({ request }) => {
+        return fetchRequestHandler({
+          req: request,
+          createContext,
+          router,
+          endpoint,
+        });
+      },
+      { parse: "none" },
+    )
+    .post(
+      `${endpoint}/*`,
+      async ({ request }) => {
+        return fetchRequestHandler({
+          req: request,
+          createContext,
+          router,
+          endpoint,
+        });
+      },
+      { parse: "none" },
+    );
+};
