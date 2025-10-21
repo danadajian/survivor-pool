bcdc39c79917a86eceb17495d7712d278d8959b5
danadajian <danadajian@gmail.com>
2025-10-21 14:12:24 -0500

    remove process exit

--- scripts/update-results/run.ts
+++ scripts/update-results/run.ts
@@ -4,5 +4,3 @@ import { fetchCurrentGames } from "../../src/pages/pool/backend";
 const { events, week, season } = await fetchCurrentGames();
 
 await updateResults(events, week.number, season.year);
-
-process.exit(0);
