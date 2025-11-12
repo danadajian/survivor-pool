0471826db1e83210be3b15836cb90d660db12331
danadajian <danadajian@gmail.com>
2025-11-12 09:45:48 -0600

    Refactor buildPickHeader function to move pick lock check inside the switch statement for improved logic flow

--- src/utils/build-pick-header.ts
+++ src/utils/build-pick-header.ts
@@ -11,20 +11,20 @@ export function buildPickHeader({
   userPick?: typeof picks.$inferSelect;
   eliminated: boolean;
 }) {
-  const pickIsLocked = checkIfPickIsLocked({
-    events,
-    userPick,
-  });
-
   if (eliminated) {
     return "Sorry, you have been eliminated from this pool.";
   }
+
   switch (userPick?.result) {
     case "WON":
       return `The ${userPick.teamPicked} won, and you're still alive!`;
     case "LOST":
       return `The ${userPick.teamPicked} lost, but you're still alive!`;
     default:
+      const pickIsLocked = checkIfPickIsLocked({
+        events,
+        userPick,
+      });
       if (pickIsLocked) {
         return `Your ${userPick?.teamPicked} pick is locked. Good luck!`;
       } else if (userPick?.teamPicked) {
