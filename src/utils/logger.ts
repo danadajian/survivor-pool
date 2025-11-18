13476918fb75fcf1316ca321c34d1996019066d8
danadajian <danadajian@gmail.com>
2025-11-18 13:45:11 -0600

    fix logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,20 @@
 import pino from "pino";
 
+import { environmentVariables } from "../env";
+
+const isDev = environmentVariables.ENVIRONMENT === "development";
+
 export const logger = pino({
   level: "info",
   timestamp: false,
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      ignore: "pid,hostname",
+  base: {}, // Remove pid, hostname, and other default fields
+  ...(isDev && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        ignore: "pid,hostname",
+      },
     },
-  },
+  }),
 });
