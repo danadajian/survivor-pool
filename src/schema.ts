878f0fffb241606af5a4d24bec8efa0c31be61ed
danadajian <danadajian@gmail.com>
2025-11-22 09:37:58 -0600

    refactor: Update schema to utilize pgEnum for result and sport fields, enhancing type safety and consistency across picks and pools tables

--- src/schema.ts
+++ src/schema.ts
@@ -8,40 +8,42 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-export const picks = pgTable("picks", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  username: varchar("username", { length: 256 }).notNull(),
-  teamPicked: varchar("team_picked", { length: 256 }).notNull(),
-  pickDate: varchar("pick_date", { length: 256 }).notNull(),
-  season: integer("season").notNull(),
-  poolId: uuid("pool_id").notNull(),
-  timestamp: timestamp("timestamp").notNull().defaultNow(),
-  pickIsSecret: boolean().default(false),
-  result: pgEnum("result", ["WON", "LOST", "PENDING"])()
-    .notNull()
-    .default("PENDING"),
-});
+export const resultEnum = pgEnum("result", ["WON", "LOST", "PENDING"]);
 
 export const SPORTS = ["NFL", "NBA", "NHL"] as const;
 export type Sport = (typeof SPORTS)[number];
 
+export const sportEnum = pgEnum("sport", SPORTS);
+
+export const picks = pgTable("picks", {
+  id: uuid().primaryKey().defaultRandom(),
+  username: varchar({ length: 256 }).notNull(),
+  teamPicked: varchar("team_picked", { length: 256 }).notNull(),
+  pickDate: varchar("pick_date", { length: 256 }).notNull(),
+  season: integer().notNull(),
+  poolId: uuid("pool_id").notNull(),
+  timestamp: timestamp().notNull().defaultNow(),
+  pickIsSecret: boolean().default(false),
+  result: resultEnum().notNull().default("PENDING"),
+});
+
 export const pools = pgTable("pools", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 256 }).notNull(),
-  creator: varchar("creator", { length: 256 }).notNull(),
-  sport: pgEnum("sport", SPORTS)().notNull().default("NFL"),
-  lives: integer("lives").notNull().default(1),
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 256 }).notNull(),
+  creator: varchar({ length: 256 }).notNull(),
+  sport: sportEnum().notNull().default("NFL"),
+  lives: integer().notNull().default(1),
   poolStart: varchar("pool_start", { length: 256 }).notNull().default(""),
   poolEnd: varchar("pool_end", { length: 256 }),
-  season: integer("season").notNull(),
+  season: integer().notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   poolWinner: varchar("pool_winner", { length: 256 }),
 });
 
 export const members = pgTable("members", {
-  id: uuid("id").primaryKey().defaultRandom(),
+  id: uuid().primaryKey().defaultRandom(),
   poolId: uuid("pool_id").notNull(),
-  username: varchar("username", { length: 256 }).notNull(),
+  username: varchar({ length: 256 }).notNull(),
   firstName: varchar("first_name", { length: 256 }),
   lastName: varchar("last_name", { length: 256 }),
 });
