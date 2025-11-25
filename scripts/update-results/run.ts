2ea38b64b3a544c1c28c370f8e00c76b093c2e2c
Dan Adajian <danadajian@gmail.com>
2025-11-25 13:50:15 -0600

    Update run.ts

--- scripts/update-results/run.ts
+++ scripts/update-results/run.ts
@@ -1,6 +1,14 @@
 import { updateResults } from "./update-results";
 import { fetchCurrentGames } from "../../src/utils/fetch-current-games";
 
-const { events, currentGameDate, currentSeason } = await fetchCurrentGames();
+const { events, currentGameDate, currentSeason } = await fetchCurrentGames("NFL");
 
 await updateResults(events, currentGameDate, currentSeason);
+
+const {
+  events: nbaEvents,
+  currentGameDate: nbaCurrentGameDate,
+  currentSeason: nbaCurrentSeason
+} = await fetchCurrentGames("NBA");
+
+await updateResults(nbaEvents, nbaCurrentGameDate, nbaCurrentSeason);
